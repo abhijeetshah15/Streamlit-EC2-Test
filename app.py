fc17ea9bfa63e4f9069f31b60e250a12566cc5ab
abhijeetshah15 <90007619+abhijeetshah15@users.noreply.github.com>
2024-09-05 11:47:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+################### How to run ###################
+# In Terminal:
+# -> python3 -m venv env && source ./env/bin/activate
+# -> streamlit run file_name.py
+##################################################
+
+st.header("Welcome to streamlit!")
